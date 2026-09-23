d1a4cb349a21d82ef2bd5293b996ccb545736a4c
Rimsha Khan <rimshaaaak20@gmail.com>
2026-09-24 02:22:27 +0530

    Time: 1 ms (99.77%), Space: 42.5 MB (61.26%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int mySqrt(int x) {
+        int low=0;   int high=x;
+        while(low<=high){
+            int mid = low + (high - low) / 2;
+            long square=(long)mid*mid;
+            if(square <= x)   low=mid+1;
+            else    high=mid-1;
+        }return high;
+    }
+}
